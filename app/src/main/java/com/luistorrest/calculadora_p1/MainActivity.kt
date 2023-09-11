99204a2835e473915b76bb0ed1af44ad1677eff7
luistorrest <luis.torres1@udea.edu.co>
2023-09-10 23:29:59 -0500

    Interfaz gráfica

---
+++ app/src/main/java/com/luistorrest/calculadora_p1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.luistorrest.calculadora_p1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
